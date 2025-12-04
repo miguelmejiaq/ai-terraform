de3359a1f00b240eaa0649c241fe04cedaa5dc0b
Miguel Mejia <mmejiaq89@gmail.com>
2025-12-04 14:52:12 -0500

    adding comment to PR

--- src/code_review.py
+++ src/code_review.py
@@ -1,10 +1,16 @@
 import requests
 import os
+import sys
 
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 OPENAI_URL = os.getenv("OPENAI_URL")
 
 CHAT_URL = f"{OPENAI_URL}/chat"
+
+output_path = os.environ.get('GITHUB_OUTPUT')
+if output_path is None:
+    print("Error: GITHUB_OUTPUT environment variable is not set.", file=sys.stderr)
+    sys.exit(1)
 
 headers = {
     "Authorization" : f"Bearer {OPENAI_KEY}",
@@ -21,4 +27,8 @@ body = {
 
 chat_request = requests.post(CHAT_URL, headers=headers, json=body)
 response = chat_request.json()
-print(response.get("choices")[0].get("message").get("content"))
+
+with open(output_path, 'a') as f:
+    # 3. Write your outputs as key=value pairs, one per line
+    f.write(f"message={response.get("choices")[0].get("message").get("content")}\n")
+print("Script finished with no issues")
