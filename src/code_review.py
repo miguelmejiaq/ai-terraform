bf203c8feabe75cb603baf93f52fd86012634d28
Miguel Mejia <mmejiaq89@gmail.com>
2025-12-04 13:29:44 -0500

    updated messages block

--- src/code_review.py
+++ src/code_review.py
@@ -13,10 +13,10 @@ headers = {
 
 body = {
     "model" : "saia:agent:716241b4-6c1c-49a4-8482-b353963108cd",
-    "messages" : {
+    "messages" : [{
         "role" : "user",
         "content" : "Hi, what can you do?"
-    }
+    }]
 }
 
 response = requests.post(CHAT_URL, headers=headers, json=body)
