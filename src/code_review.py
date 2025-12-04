8acfd6c8ffe63f2fcc288ed1eb28ccc4f7f71af7
Miguel Mejia <mmejiaq89@gmail.com>
2025-12-04 13:24:30 -0500

    added secrets to key

--- src/code_review.py
+++ src/code_review.py
@@ -1,3 +1,24 @@
 import requests
+import os
 
-print("python works")
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+OPENAI_URL = os.getenv("OPENAI_URL")
+
+CHAT_URL = f"{OPENAI_URL}/chat"
+
+headers = {
+    "Authorization" : f"Bearer {OPENAI_KEY}",
+    "Content-Type": "application/json"
+}
+
+body = {
+    "model" : "saia:agent:716241b4-6c1c-49a4-8482-b353963108cd",
+    "messages" : {
+        "role" : "user",
+        "content" : "Hi, what can you do?"
+    }
+}
+
+response = requests.post(url, headers=headers, json=body)
+
+print(response.json())
